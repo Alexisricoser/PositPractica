78750ace5990074eaa9cc6cc1b7ff43dfc14b647
samuel <samuelantoniocordovarojas@gmail.com>
2025-09-26 16:56:08 +0200

    intento de javascript

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
 
 localStorage.removeItem("postit", null);
+let doc=document
+let form=doc.getElementById("formulario");
+let input=doc.getElementById("contenido");
+let container=doc.getElementById("notasgrid")
 
-let lista = ["fdsf","dsfsdf"];
+let lista = [];
 
-localStorage.setItem("lista_de_notas", JSON.stringify(lista));
 
-let arrayRecuperada = JSON.parse(localStorage.getItem("lista_de_notas"))
 
-console.log(arrayRecuperada[0]);
+
+
+
+
 
 botonenviar.addEventListener("click"   ,  (evento) => {
   evento.preventDefault();
   let div = document.createElement("div");
+  div.className="notas"
+  let botonBorrar=document.createElement("button")
+  botonBorrar.className="borrar"
   div.innerText = contenido.value;
   notasgrid.appendChild(div);
+  botonBorrar.innerText="x"
+  div.appendChild(botonBorrar)
+  lista.push(contenido.value)
+  localStorage.setItem("postit", JSON.stringify(lista));
+  let contenidoRecuperada=JSON.parse(localStorage.getItem("postit"))
+  
 
-  localStorage.setItem("postit", contenido.value);
+  botonBorrar.addEventListener("click",(evento)=>{
+    container.removeChild(div)
+  })
+
+  
 });
+
+
+
+
+
+
+
+
+
+
+
 
 
