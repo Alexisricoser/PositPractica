b546042e09aa63561422726b0556b18851a3be3e
alericser <alericser@alu.edu.gva.es>
2025-10-17 15:14:00 +0200

    a

--- script.js
+++ script.js
@@ -8,6 +8,12 @@
 
     let div = document.createElement("div");
     div.innerText = contenido.value;
+    div.id= 'item' + postit.length
+    const botonEditar = document.createElement("button");
+    botonEditar.className = "Editar";
+    botonEditar.innerText = "Editar";
+    botonEditar.addEventListener('click', editar(div.id))
+    div.appendChild(botonEditar);
     notasgrid.appendChild(div);
 
   });
@@ -16,8 +22,18 @@
     for(let i = 0 ; i < postit.length; i++){
       let div = document.createElement("div");
       div.innerText = postit[i];
+      const botonEditar = document.createElement("button");
+      botonEditar.className = "Editar";
+      botonEditar.innerText = "Editar";
+      div.appendChild(botonEditar);
       notasgrid.appendChild(div);
+
     }
   };
+
+  function editar(id){
+    let contenido = prompt('Texto a cambiar');
+    id.innerText=contenido
+  }
 
   cargarPostit();
